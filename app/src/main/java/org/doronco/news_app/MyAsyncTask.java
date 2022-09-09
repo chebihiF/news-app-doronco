e35821f48d9e4aa5b8e40793e184244d4b90e34c
chebihiF <chebihi.f@ucd.ac.ma>
2022-09-09 10:01:00 +0100

    Initial commit

---
+++ app/src/main/java/org/doronco/news_app/MyAsyncTask.java
@@ -0,0 +1,12 @@
+package org.doronco.news_app;
+
+import android.os.AsyncTask;
+
+public class MyAsyncTask extends AsyncTask<String,Integer,String> {
+
+    @Override
+    protected String doInBackground(String... strings) {
+
+        return null;
+    }
+}
